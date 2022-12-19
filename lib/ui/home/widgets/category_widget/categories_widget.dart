5084209cb72765ed887084e0a28e2a389bdc1205
collo <mutaic84@gmail.com>
2022-12-19 22:30:41 +0300

    :hammer: add widget to display categories data on success

--- lib/ui/home/widgets/category_widget/categories_widget.dart
+++ lib/ui/home/widgets/category_widget/categories_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:games_info/ui/home/widgets/category_widget/bloc/category_bloc.dart';
+import 'package:games_info/ui/home/widgets/category_widget/categories_success_widget.dart';
 
 class CategoriesWidget extends StatelessWidget {
   const CategoriesWidget({super.key});
@@ -10,7 +11,7 @@ class CategoriesWidget extends StatelessWidget {
     return BlocBuilder<CategoryBloc, CategoryState>(
       buildWhen: ((previous, current) => current.status.isSuccess),
       builder: (context, state) => 
-        return CategoriesSuccessWidget();
+        const CategoriesSuccessWidget()
     );
   }
 }
