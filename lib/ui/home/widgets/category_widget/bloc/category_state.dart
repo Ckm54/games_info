66b4a2dc19826d236003a20f1ed4cc0ae855b213
collo <mutaic84@gmail.com>
2022-12-19 21:39:03 +0300

    :hammer: define category state

--- lib/ui/home/widgets/category_widget/bloc/category_state.dart
+++ lib/ui/home/widgets/category_widget/bloc/category_state.dart
@@ -1,3 +1,5 @@
+import 'package:equatable/equatable.dart';
+
 part 'category_bloc.dart';
 
 enum CategoryStatus { initial, success, error, loading, selected }
@@ -8,4 +10,30 @@ extension CategoryStatusX on CategoryStatus {
   bool get isError => this == CategoryStatus.error;
   bool get isLoading => this == CategoryStatus.loading;
   bool get isSelected => this == CategoryStatus.selected;
+}
+
+class CategoryState extends Equatable {
+  const CategoryState({
+    this.status = CategoryStatus.initial,
+    List<Genre>? categories,
+    this.idSelected = 0,
+  }) : categories = categories ?? const [];
+  final List<Genre> categories;
+  final CategoryStatus status;
+  final int idSelected;
+
+  @override
+  List<Object?> get props => [status, categories, idSelected];
+
+  CategoryState copyWith({
+    List<Genre>? categories,
+    CategoryStatus? status,
+    int? idSelected,
+  }) {
+    return CategoryState(
+      categories: categories ?? this.categories,
+      status: status ?? this.status,
+      idSelected: idSelected ?? this.idSelected,
+    );
+  }
 }
