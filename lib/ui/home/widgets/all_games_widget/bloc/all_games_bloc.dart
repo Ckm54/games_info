ed975ffc61da1ca70d591158528fa53736e38520
collo <mutaic84@gmail.com>
2022-12-27 08:59:51 +0300

    :hammer: complete all games state

---
+++ lib/ui/home/widgets/all_games_widget/bloc/all_games_bloc.dart
@@ -0,0 +1,6 @@
+import 'package:equatable/equatable.dart';
+
+import '../../../../../repository/models/game.dart';
+
+part 'all_games_state.dart';
+part 'all_games_event.dart';
---
+++ lib/ui/home/widgets/all_games_widget/bloc/all_games_state.dart
@@ -0,0 +1,35 @@
+// create extension to handle all possible states and a copywith method to create a new copy of the object when needed
+
+part of 'all_games_bloc.dart';
+
+enum AllGamesStatus { initial, success, error, loading }
+
+extension AllGamesStatusX on AllGamesStatus {
+  bool get isInitial => this == AllGamesStatus.initial;
+  bool get isSuccess => this == AllGamesStatus.success;
+  bool get isError => this == AllGamesStatus.error;
+  bool get isLoading => this == AllGamesStatus.loading;
+}
+
+class AllGamesState extends Equatable {
+  const AllGamesState({
+    this.status = AllGamesStatus.initial,
+    Game? games,
+  }) : games = games ?? Game.empty;
+
+  final Game games;
+  final AllGamesStatus status;
+
+  @override
+  List<Object?> get props => [status, games];
+
+  AllGamesState copyWith({
+    Game? games,
+    AllGamesStatus? status,
+  }) {
+    return AllGamesState(
+      games: games ?? this.games,
+      status: status ?? this.status,
+    );
+  }
+}
