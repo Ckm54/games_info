1b757f0ab19933fabbe26b53c4e60a4033458a04
collo <mutaic84@gmail.com>
2022-12-28 10:17:22 +0300

    :hammer: gamesbycategory widget updates

--- lib/ui/home/widgets/games_by_category_widget/game_by_category_barrel.dart
+++ lib/ui/home/widgets/games_by_category_widget/game_by_category_barrel.dart
@@ -1,4 +1,5 @@
+export 'bloc/games_by_category_bloc.dart';
 export 'game_by_category_image.dart';
-export 'game_by_category_success_widget.dart';
 export 'game_by_category_title.dart';
+export 'game_by_category_success_widget.dart';
 //  game categories barrel file
